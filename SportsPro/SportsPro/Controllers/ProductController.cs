ce0273d9e01a8bc234f842158cdd2c3384a9bfdd
Remnel831 <136384272+Remnel831@users.noreply.github.com>
2025-01-30 15:55:52 -0600

    Started adding views

---
+++ SportsPro/SportsPro/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsPro.Models;
+
+namespace SportsPro.Controllers
+{
+	public class ProductController : Controller
+	{
+		//Add
+
+		[HttpGet]
+		public IActionResult Add()
+		{
+			//
+			return RedirectToAction("List", "Product");
+		}
+
+
+		//Edit
+		[HttpGet]
+		public IActionResult Edit(int id)
+		{
+			ViewBag.Action = "Edit";
+			// var product = context.Products.Find(id);
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Edit(Product product) 
+		{
+			if (ModelState.IsValid)
+			{
+				return RedirectToAction("List", "Product");
+			}
+
+			else 
+			{
+				return View();
+			}
+		}
+		//List
+		public IActionResult List() 
+		{
+			//var products = 
+			return View();
+		}
+	}
+}
--- SportsPro/SportsPro/obj/Debug/net6.0/SportsPro.AssemblyInfo.cs
+++ SportsPro/SportsPro/obj/Debug/net6.0/SportsPro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a8b381bd2719be432bbd6351d117c235e4c02c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

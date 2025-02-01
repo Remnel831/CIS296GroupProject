8f5770a2c2af083f7b308ed67b3825a7a3c59588
GingerThor <136877640+GingerThor@users.noreply.github.com>
2025-02-01 11:22:55 -0600

    Added Technician controller Fixed a couple of bugs in views for the technician and added delete view for technicians

---
+++ SportsPro/SportsPro/Controllers/TechnicianController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsPro.Models;
+
+namespace SportsPro.Controllers
+{
+    public class TechnicianController : Controller
+    {
+        private SportsProContext Context { get; set; }
+
+        public TechnicianController(SportsProContext ctx)
+        {
+            Context = ctx;
+        }
+
+        //Add
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Edit";
+            return View("Edit", new Technician());
+        }
+
+        //Edit
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            var technician = Context.Technicians.Find(id);
+            return View(technician);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Technician technician)
+        {
+            if (ModelState.IsValid)
+            {
+                if (technician.TechnicianID == 0)
+                {
+                    Context.Technicians.Add(technician);
+                    Context.SaveChanges();
+                    return RedirectToAction("List", "Technician");
+                }
+                else
+                {
+                    Context.Technicians.Update(technician);
+                    Context.SaveChanges();
+                    return RedirectToAction("List", "Technician");
+                }
+
+            }
+            else
+            {
+                ViewBag.Action = (technician.TechnicianID == 0) ? "Add" : "Edit";
+                return View(technician);
+            }
+        }
+
+        //List
+        public IActionResult List()
+        {
+            var technicians = Context.Technicians.ToList();
+            return View(technicians);
+        }
+
+        //Delete
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var technician = Context.Technicians.Find(id);
+            return View(technician);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Technician technician)
+        {
+            ViewBag.Action = "Delete";
+            Context.Technicians.Remove(technician);
+            Context.SaveChanges();
+            return RedirectToAction("List", "Technician");
+        }
+    }
+}
--- SportsPro/SportsPro/obj/Debug/net6.0/SportsPro.AssemblyInfo.cs
+++ SportsPro/SportsPro/obj/Debug/net6.0/SportsPro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f961f51638193be3c2217ab95a028beb305399ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+343108b72a34b6d4287b8052f7a009467ad829ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

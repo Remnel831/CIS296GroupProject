95c2840f7f9ace82b0759b2d53dc7bcaeb32312b
Malachi Harris <malachi.harris@southeasttech.edu>
2025-02-01 14:13:55 -0600

    Added tags to customize validation summary  message to account for -1 yearly cost of product, and to ensure proper spacing on product code

--- SportsPro/SportsPro/Models/Product.cs
+++ SportsPro/SportsPro/Models/Product.cs
@@ -1,14 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SportsPro.Models
 {
     public class Product
     {
 		public int ProductID { get; set; }
+        //correctly spaces "Product code"
+        [Required(ErrorMessage = "The Product code field is required")]
 		public string ProductCode { get; set; } = string.Empty;
 		public string Name { get; set; } = string.Empty;
-		[Column(TypeName = "decimal(8,2)")]
-		public decimal YearlyPrice { get; set; }
-		public DateTime ReleaseDate { get; set; } = DateTime.Now;
+        [Column(TypeName = "decimal(8,2)")]
+        //account for negative entry
+        [Range(0.01, 99999999.99, ErrorMessage = "Yearly price must be a positive number.")]
+        public decimal YearlyPrice { get; set; }
+        public DateTime ReleaseDate { get; set; } = DateTime.Now;
 	}
 }

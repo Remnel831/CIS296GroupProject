4c4231b9eb19fd8f8711f991239130661382bccd
GingerThor <136877640+GingerThor@users.noreply.github.com>
2025-02-08 12:22:50 -0600

    Added InidentController and IncidentViewModel Updated Incident Edit View Added Validation to Incidnet Model

---
+++ SportsPro/SportsPro/Models/IncidentViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SportsPro.Models
+{
+    public class IncidentViewModel
+    {
+        public Incident Incident { get; set; } = null!;
+        public IEnumerable<Technician> Technicians { get; set; } = new List<Technician>();
+        public IEnumerable<Customer> Customers { get; set; } = new List<Customer>();
+        public IEnumerable<Product> Products { get; set; } = new List<Product>();
+
+        public IncidentViewModel() { }
+    }
+}

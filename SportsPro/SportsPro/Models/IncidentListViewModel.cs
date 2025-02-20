39f1ecdd1d4c99acc07bbe0c2d8a31fd753c42a8
GingerThor <136877640+GingerThor@users.noreply.github.com>
2025-02-19 20:11:14 -0600

    Update To IncidentViewModel and Added Incident List View Model

---
+++ SportsPro/SportsPro/Models/IncidentListViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace SportsPro.Models
+{
+    public class IncidentListViewModel
+    {
+        public string IncidentFilter { get; set; } = IncidentFilterEnum.All.ToString();
+        public List<Incident> Incidents { get; set; } = null!;
+    }
+
+    public enum IncidentFilterEnum
+    {
+        All,
+        Open,
+        Unassigned
+    }
+}

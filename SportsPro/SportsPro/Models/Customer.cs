390ed856b82942401f9a9a7058f4f99af196d5cd
Remnel831 <136384272+Remnel831@users.noreply.github.com>
2025-04-21 21:40:02 -0500

    Added validation to customer and added css for input validation errors.

--- SportsPro/SportsPro/Models/Customer.cs
+++ SportsPro/SportsPro/Models/Customer.cs
@@ -4,28 +4,38 @@ namespace SportsPro.Models
 {
     public class Customer
     {
-		public int CustomerID { get; set; }
+        public int CustomerID { get; set; }
         [Required(ErrorMessage = "Please enter the customer's first name.")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "First name must be between 1 and 50 characters.")]
         public string FirstName { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Please enter the customer's last name.")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Last name must be between 1 and 50 characters.")]
         public string LastName { get; set; } = string.Empty;
+
         [Required(ErrorMessage = "Please enter the customer's address.")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Customer's address must be between 1 and 50 characters.")]
         public string Address { get; set; } = string.Empty;
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Customer's city must be between 1 and 50 characters.")]
         [Required(ErrorMessage = "Please enter the customer's City.")]
         public string City { get; set; } = string.Empty;
+
         [Required(ErrorMessage = "Please enter the customer's State.")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Customer's state must be between 1 and 50 characters.")]
         public string State { get; set; } = string.Empty;
+
         [Required(ErrorMessage = "Please enter the customer's postal code.")]
+        [StringLength(20, MinimumLength = 1, ErrorMessage = "Customer's zip code must be between 1 and 20 characters.")]
         public string PostalCode { get; set; } = string.Empty;
        
-        [RegularExpression("^\\s*(\\+1\\s*)?(?:\\(?\\d{3}\\)?([\\s.-])*?)?\\d{3}[\\s.-]*?\\d{4}\\s*$", ErrorMessage = "Please enter a valid Phone number")]
+        [RegularExpression(@"^\(\d{3}\)\d{3}-\d{4}$", ErrorMessage = "Phone numbers must match the format (555)555-5555")]
         public string? Phone { get; set; }
-       
-        [RegularExpression("^(?!.*\\.\\.)(?!\\.)[a-zA-Z0-9]([a-zA-Z0-9_+\\-\\.]{0,62}[a-zA-Z0-9])?[@](?=.{4,255}$)[a-zA-Z0-9]+([.-][a-zA-Z0-9]+)*\\.(xn--)?[a-zA-Z0-9]{2,}$", ErrorMessage = "Please enter a valid email.")]
+
+        [DataType(DataType.EmailAddress)]
+        [NewEmail]
         public string? Email { get; set; }
 
-        [Required(ErrorMessage ="Please select a country.")]
+        [ValidCountry]
         public string CountryID { get; set; } = string.Empty;   // foreign key property
         public Country? Country { get; set; } = null!;           // navigation property
 
